80e4de4a1f5a221b02dc937a6fcc51691ad5144e
unknown <harshadhatagale6@gmail.com>
2024-07-31 11:21:10 +0530

    First commit

--- src/components/ui/Nav.jsx
+++ src/components/ui/Nav.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
-
+import Link from 'next/link'
+import { Poppins } from 'next/font/google';
+import { LuClipboardList } from "react-icons/lu";
+import { FaGithub } from "react-icons/fa";
+const poppins= Poppins({
+    subsets: ['latin'],
+   weight: ["100",'200','300','400','500','600','700','800','900']
+})
 export default function Nav() {
   return (
-    <div>
-        <ul>
-            
-        </ul>
+    <div className='select-none flex w-full h-20 justify-around dark:text-white text-black bg-white dark:bg-slate-950 items-center'>
+        <div className='text-2xl font-bold'>
+            <Link href={"/"} className='flex space-x-2 justify-center items-center'><LuClipboardList /><span>Todo Listify</span></Link>
+        </div>
+        <div className='text-2xl font-bold'>
+          <Link href={"/"}><FaGithub /></Link>
+        </div>
     </div>
   )
 }
